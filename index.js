21bf1091b39519b594ade028ca935a1b453f3059
mimiring <island_2@naver.com>
2019-09-23 13:45:01 +0000

    구름IDE 이동

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+const PORT = 4000;
+
+const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
+const handleHome = (req, res) => res.send('Hello from Home!!');
+const handleProfile = (req, res) => res.send("you are!");
+
+app.get("/", handleHome);
+app.get("/profile", handleProfile);
+app.listen(PORT, handleListening);
