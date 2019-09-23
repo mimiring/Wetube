858abc54cea4e65884e2521c5f95f421bd7e6cb6
mimiring <island_2@naver.com>
2019-09-23 15:25:53 +0000

    middleware 추가

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 import express from "express";
+//const express = require("express");
 
 const app = express();
 
 const PORT = 4000;
 
 const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
-const handleHome = (req, res) => res.send('Hello from Home!!');
-const handleProfile = (req, res) => res.send("you are!");
+const handleHome = (req, res) => res.send('Hello from home!!');
+const handleProfile = (req, res) => res.send("You are on my profile.");
 
+const betweenHome = (req, res, next) => {
+	console.log("I am super cooool!!");
+	next();
+};
+
+
+app.use(betweenHome);
 app.get("/", handleHome);
 app.get("/profile", handleProfile);
+
+
 app.listen(PORT, handleListening);
