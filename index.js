6938b4a7643d27f9b034b1c8e62ce73a6f6451a0
mimiring <island_2@naver.com>
2019-09-24 15:02:46 +0000

    add middleware(cookie-parser, body-parser)

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 import express from "express";
 //const express = require("express");
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 
 const app = express();
 
@@ -9,15 +13,14 @@ const handleListening = () => console.log(`Listening on: http://localhost:${PORT
 const handleHome = (req, res) => res.send('Hello from home!!');
 const handleProfile = (req, res) => res.send("You are on my profile.");
 
-const betweenHome = (req, res, next) => {
-	console.log("I am super cooool!!");
-	next();
-};
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(helmet());
+app.use(morgan("dev"));
 
-
-app.use(betweenHome);
 app.get("/", handleHome);
 app.get("/profile", handleProfile);
 
 
-app.listen(PORT, handleListening);
+app.listen(PORT, handleListening);	
