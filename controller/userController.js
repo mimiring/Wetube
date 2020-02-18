fbb77ebd21f7cdeaf5e3ff6ad9351bcf228a57ba
mimiring <island_2@naver.com>
2020-02-18 21:53:12 +0900

    upload (upload 화면, upload 후 해당 id 화면으로 이동)

--- controller/userController.js
+++ controller/userController.js
@@ -22,15 +22,20 @@ export const postJoin = (req, res) => {
 	}	else {
 		// To Do : Register User
 		// To Do : Log user in
-		res.redirect(routes.home)
+		res.redirect(routes.home);
 	}
 };
 
 export const getLogin = (req, res) => res.render("login", { pageTitle: "Login" });
 export const postLogin = (req, res) => {
 	res.redirect(routes.home);
-}
-export const logout = (req, res) => res.render("logout", { pageTitle: "Logout" });
+};
+
+export const logout = (req, res) => {
+	// To Do: Process Log Out
+	res.redirect(routes.home);
+};
+
 export const users = (req, res) => res.render("Users", { pageTitle: "Users" });
 export const userDetail = (req, res) => res.render("userDetail", { pageTitle: "User Detail" });
 export const editProfile = (req, res) => res.render("editProfile", { pageTitle: "Edit Profile" });
