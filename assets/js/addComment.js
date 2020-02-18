d93626cb0ec9a300af9b2f7f812db06805e584ff
mimiring <island_2@naver.com>
2020-02-18 21:53:39 +0900

    fake comment(database에 올리고 업데이트하면 오래 걸리므로)

--- assets/js/addComment.js
+++ assets/js/addComment.js
@@ -1,5 +1,21 @@
 import axios from "axios";
 const addCommentForm = document.getElementById("jsAddComment");
+const commentList = document.getElementById("jsCommentList");
+const commentNumber = document.getElementById("jsCommentNumber");
+
+const increaseNumber = () => {
+  commentNumber.innerHTML = parseInt(commentNumber.innerHTML, 10) +1;
+  //commentNumber가 들어있는 게 String이라서 parseInt로 숫자로 바꿔줌
+}
+
+const addComment = comment => {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  span.innerHTML = comment;
+  li.appendChild(span);
+  commentList.prepend(li);
+  increaseNumber();
+};
 
 const sendComment = async comment => {
   const videoId = window.location.href.split("/videos/")[1];
@@ -10,7 +26,10 @@ const sendComment = async comment => {
       comment
     }
   });
-  console.log(response);
+  if(response.status === 200) {
+    addComment(comment); //댓글이 데이터베이스에 추가되면 addComment
+  }
+
 };
 
 const handleSubmit = event => {
