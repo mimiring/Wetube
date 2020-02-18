4cbee9466f29ae2c7ba8473b1a46420bd899667a
mimiring <island_2@naver.com>
2020-02-18 21:53:26 +0900

    session으로 사용자 정보 저장

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
 import passport from "passport";
+import session from "express-session";
 import { localsMiddleware } from "./middlewares";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
@@ -39,6 +40,11 @@ app.use(cookieParser());
 app.use(bodyParser.json()); // { "name": "mimiring" }
 app.use(bodyParser.urlencoded({ extended: true })); // form 에서 오는 데이터 extended의 기본값이 true / true qs로 쿼리문을 해석하겠다. false querystring으로 해석한다.
 app.use(morgan("dev")); // log 두도잘몰라
+app.use(session({
+    secret: process.env.COOKIE_SECRET,
+    resave: true,
+    saveUninitialized: false
+}))
 app.use(passport.initialize());
 app.use(passport.session());
 
--- middlewares.js
+++ middlewares.js
@@ -6,7 +6,7 @@ const multerVideo = multer({ dest: "uploads/videos/" });
 export const localsMiddleware = (req, res, next) => {
 	res.locals.siteName = ":: WeTube";
 	res.locals.routes = routes;
-	res.locals.user = req.user || {}; // user가 존재하지 않으면 빈 object를 줌
+	res.locals.user = req.user || null; // user가 존재하지 않으면 빈 object를 줌
 	next();
 };
 
