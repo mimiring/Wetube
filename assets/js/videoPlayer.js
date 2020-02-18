81819fcc3894fdff740e5f325b5ea53bac824f35
mimiring <island_2@naver.com>
2020-02-18 21:53:34 +0900

    video current time/total time

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -3,6 +3,8 @@ const videoPlayer = document.querySelector("#jsVideoPlayer video"); //여러 함
 const playBtn = document.getElementById("jsPlayButton");
 const volumeBtn = document.getElementById("jsVolumeBtn");
 const fullScrnBtn = document.getElementById("jsFullScreen");
+const currentTime = document.getElementById("currentTime");
+const totalTime = document.getElementById("totalTime");
 
 function handlePlayClick() {
     if(videoPlayer.paused) {
@@ -53,10 +55,42 @@ function goFullScreen() {
     fullScrnBtn.addEventListener("click", exitFullScreen);
 }
 
+const formatDate = seconds => { //시간이 주어졌을 때 00:00:00 형식으로 변환하는 함수(ex 10000초)
+    const secondsNumber = parseInt(seconds, 10); // 초를 10진법 정수로 설정
+    let hours = Math.floor(secondsNumber / 3600); // 1시간은 3600초(2시간)
+    let minutes = Math.floor((secondsNumber - hours * 3600) / 60); // 1분은 60초(46분)
+    let totalSeconds = secondsNumber - hours * 3600 - minutes * 60; // (40초. total 02:46:40)
+
+    if(hours < 10) {
+        hours = `0${hours}`;
+    }
+    if(minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+    if(seconds < 10) {
+        totalSeconds = `0${totalSeconds}`;
+    }
+    return `${hours}:${minutes}:${totalSeconds}`;
+};
+
+function getCurrentTime() {
+    currentTime.innerHTML = formatDate(videoPlayer.currentTime);
+}
+
+function setTotalTime() { 
+    const totalTimeString = formatDate(videoPlayer.duration);
+    totalTime.innerHTML = totalTimeString;
+    setInterval(getCurrentTime, 1000);// time get은 한 번만 일어나는 이벤트이므로 매 초마다 갱신해줌
+}
+
+
+
 function init() {
     playBtn.addEventListener("click", handlePlayClick);
     volumeBtn.addEventListener("click", handleVolumeClick);
     fullScrnBtn.addEventListener("click", goFullScreen);
+    setTotalTime();
+        //metadata가 video가 load 될 때까지 기다렸다가 시간 표기해줌(totalTime을 얻으면 video가 load된 것임)
 }
 
 if(videoContainer) {
