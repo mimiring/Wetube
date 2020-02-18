e1ee0c8ae0c013109e11d1f19e3c5e1c8b2609be
mimiring <island_2@naver.com>
2020-02-18 21:53:27 +0900

    로그인용 화면 설정

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -7,24 +7,20 @@ import {
 	logout,
 	postJoin,
 	getLogin,
-	postLogin
+	postLogin,
 } from "../controller/userController";
+import {onlyPublic} from '../middlewares';
 
 const globalRouter = express.Router();
 
-globalRouter.get(routes.join, getJoin);
-globalRouter.post(routes.join, postJoin, postLogin); //회원가입하면 로그인 화면으로 넘어감
+globalRouter.get(routes.join, onlyPublic, getJoin);
+globalRouter.post(routes.join, onlyPublic, postJoin, postLogin); //회원가입하면 로그인 화면으로 넘어감
+
+globalRouter.get(routes.login, onlyPublic, getLogin);
+globalRouter.post(routes.login, onlyPublic, postLogin);
 
 globalRouter.get(routes.home, home);
 globalRouter.get(routes.search, search);
-
-globalRouter.get(routes.login, getLogin);
-globalRouter.post(routes.login, (req,res,next) => {
-	console.log(req.body);
-	next();
-},postLogin);
-
 globalRouter.get(routes.logout, logout);
-
 
 export default globalRouter;
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -9,19 +9,20 @@ import {
 	postEditVideo
 } from "../controller/videoController";
 import { uploadVideo } from "../middlewares";
+import { onlyPrivate } from "../middlewares";
 
 const videoRouter = express.Router();
 
 //Upload
-videoRouter.get(routes.upload, getUpload);
-videoRouter.post(routes.upload, uploadVideo, postUpload);
+videoRouter.get(routes.upload, onlyPrivate, getUpload);
+videoRouter.post(routes.upload, onlyPrivate,  uploadVideo, postUpload);
 
 //Video Detail
 videoRouter.get(routes.videoDetail(), videoDetail);
 
 //Edit Video
-videoRouter.get(routes.editVideo(), getEditVideo);
-videoRouter.post(routes.editVideo(), postEditVideo);
+videoRouter.get(routes.editVideo(), onlyPrivate,  getEditVideo);
+videoRouter.post(routes.editVideo(), onlyPrivate,  postEditVideo);
 
 //Delete Video
 videoRouter.get(routes.deleteVideo(), deleteVideo);
