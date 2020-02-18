8e0b9f682a01d8d95602b07cc846445f9657c9b0
mimiring <island_2@naver.com>
2020-02-18 21:53:37 +0900

    video recording - stop, save

--- assets/js/videoRecorder.js
+++ assets/js/videoRecorder.js
@@ -2,28 +2,55 @@ const recorderContainer = document.getElementById("jsRecordContainer");
 const recordBtn = document.getElementById("jsRecordBtn");
 const videoPreview = document.getElementById("jsVideoPreview");
 
-const startRecording = async() => {
-    try {
-        const stream = await navigator.mediaDevices.getUserMedia({
-            audio: true,
-            video: { width: 1280, hegiht: 720 }
-        });
-        videoPreview.srcObject = stream;
-        videoPreview.muted = true; //녹화 시 목소리가 안들리게
-        videoPreview.play();
-        
-        console.log(stream);
-    } catch (error) {
-        recordBtn.innerHTML = "☹️ Can't record";
-        recordBtn.removeEventListener("click", startRecording);
-    }
-    
-}
+let streamObject;
+let videoRecorder;
+
+const handleVideoData = event => {
+  const { data: videoFile } = event;
+  const link = document.createElement("a");
+  link.href = URL.createObjectURL(videoFile);
+  link.download = "recorded.webm";
+  document.body.appendChild(link);
+  link.click();
+};
+
+const stopRecording = () => {
+  videoRecorder.stop();
+  recordBtn.removeEventListener("click", stopRecording);
+  recordBtn.addEventListener("click", getVideo);
+  recordBtn.innerHTML = "Start recording";
+};
+
+const startRecording = () => {
+  videoRecorder = new MediaRecorder(streamObject);
+  videoRecorder.start();
+  videoRecorder.addEventListener("dataavailable", handleVideoData);
+  recordBtn.addEventListener("click", stopRecording);
+};
+
+const getVideo = async () => {
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia({
+      audio: true,
+      video: { width: 1280, height: 720 }
+    });
+    videoPreview.srcObject = stream;
+    videoPreview.muted = true;
+    videoPreview.play();
+    recordBtn.innerHTML = "Stop recording";
+    streamObject = stream;
+    startRecording();
+  } catch (error) {
+    recordBtn.innerHTML = "☹️ Cant record";
+  } finally {
+    recordBtn.removeEventListener("click", getVideo);
+  }
+};
 
 function init() {
-    recordBtn.addEventListener("click", startRecording);
+  recordBtn.addEventListener("click", getVideo);
 }
 
-if(recorderContainer) {
-    init();
+if (recorderContainer) {
+  init();
 }
