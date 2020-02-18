b451cef9a6435584ff031911d469f74c774d8048
mimiring <island_2@naver.com>
2020-02-18 21:53:11 +0900

    join 화면 작동

--- controller/userController.js
+++ controller/userController.js
@@ -1,4 +1,6 @@
-export const join = (req, res) => {
+import routes from "../routes";
+
+export const getJoin = (req, res) => {
 	// 회원가임
 	// db[req.body.id] = req.body.password;
 	// if(db.includes(req.body.id)){
@@ -9,10 +11,25 @@ export const join = (req, res) => {
 	// }
 	res.render("join", { pageTitle: "Join" });
 };
+
+export const postJoin = (req, res) => {
+	const {
+		body : { name, email, password, password2 }
+	} = req;
+	if(password != password2) {
+		res.status(400);
+		res.render("join", { pageTitle: "Join" });
+	}	else {
+		// To Do : Register User
+		// To Do : Log user in
+		res.redirect(routes.home)
+	}
+};
+
 export const login = (req, res) => res.render("login", { pageTitle: "Login" });
-export const logout = (req, res) => res.send("logout", { pageTitle: "Logout" });
-export const users = (req, res) => res.send("Users", { pageTitle: "Users" });
-export const userDetail = (req, res) => res.send("userDetail", { pageTitle: "User Detail" });
+export const logout = (req, res) => res.render("logout", { pageTitle: "Logout" });
+export const users = (req, res) => res.render("Users", { pageTitle: "Users" });
+export const userDetail = (req, res) => res.render("userDetail", { pageTitle: "User Detail" });
 export const editProfile = (req, res) => res.render("editProfile", { pageTitle: "Edit Profile" });
 export const changePassword = (req, res) => res.render("changePassword", { pageTitle: "Change Password" });
 
--- controller/videoController.js
+++ controller/videoController.js
@@ -9,7 +9,7 @@ export const search = (req, res) => {
 		query: { term: searchingBy }
 	} = req;
 	//const searchingBy = req.query.term;
-	res.render("search", { pageTitle: "Search", searchingBy });
+	res.render("search", { pageTitle: "Search", searchingBy, videos });
 };
 
 export const upload = (req, res) => res.render("upload", { pageTitle: "Upload" });
