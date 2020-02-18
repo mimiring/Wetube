2444d9712e0e8d6868fe2d6223bb245d51b01593
mimiring <island_2@naver.com>
2020-02-18 21:53:11 +0900

    mixin

---
+++ db.js
@@ -0,0 +1,51 @@
+export const videos = [
+	{
+	id: 232,
+	title: "Awesome",
+	description : "This is sometin what I love",
+	views: 25,
+	videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+	creator: {
+		name: "Mimi",
+		id: 121212,
+		email: "mimi@hey.com",
+		
+		}
+	},
+	{
+	id: 1234,
+	title: "Perfect",
+	description : "This is sometin what I love",
+	views: 15,
+	videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+	creator: {
+		name: "Mimi",
+		id: 121212,
+		email: "mimi@hey.com",
+		
+		}
+	},{
+	id: 47434,
+	title: "Super",
+	description : "This is sometin what I love",
+	views: 35,
+	videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+	creator: {
+		name: "Mimi",
+		id: 121212,
+		email: "mimi@hey.com",
+		
+	}
+}, {
+	id: 809145,
+	title: "Cool",
+	description : "This is sometin what I love",
+	views: 37,
+	videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+	creator: {
+		name: "Mimi",
+		id: 121212,
+		email: "mimi@hey.com",
+		
+	}
+}];
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -10,6 +10,7 @@ import {
 
 const videoRouter = express.Router();
 
+
 videoRouter.get(routes.upload, upload);
 videoRouter.get(routes.videoDetail, videoDetail);
 videoRouter.get(routes.editVideo, editVideo);
