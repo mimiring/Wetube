54eba037ebfe2172c07e5933a2b76b02585691d4
mimiring <island_2@naver.com>
2020-02-18 21:53:34 +0900

    video fullscreen 설정/해제

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -2,16 +2,7 @@ const videoContainer = document.getElementById("jsVideoPlayer");
 const videoPlayer = document.querySelector("#jsVideoPlayer video"); //여러 함수에서 사용해야 함
 const playBtn = document.getElementById("jsPlayButton");
 const volumeBtn = document.getElementById("jsVolumeBtn");
-
-function handleVolumeClick() {
-    if(videoPlayer.muted) {
-        videoPlayer.muted = false;
-        volumeBtn.innerHTML = '<i class="fas fa-volume-up"></i>'
-    } else {
-        videoPlayer.muted = true;
-        volumeBtn.innerHTML = '<i class="fas fa-volume-mute"></i>'
-    }
-}
+const fullScrnBtn = document.getElementById("jsFullScreen");
 
 function handlePlayClick() {
     if(videoPlayer.paused) {
@@ -23,9 +14,49 @@ function handlePlayClick() {
     }
 }
 
+function handleVolumeClick() {
+    if(videoPlayer.muted) {
+        videoPlayer.muted = false;
+        volumeBtn.innerHTML = '<i class="fas fa-volume-up"></i>'
+    } else {
+        videoPlayer.muted = true;
+        volumeBtn.innerHTML = '<i class="fas fa-volume-mute"></i>'
+    }
+}
+
+function exitFullScreen() {
+    fullScrnBtn.innerHTML = '<i class="fas fa-expand"></i>';
+    fullScrnBtn.addEventListener("click", goFullScreen);
+    if(document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if(document.webkitExitFullscreen) {
+        document.webkitExitFullscreen();
+    } else if(document.mozExitFullscreen) {
+        document.mozExitFullscreen();
+    } else if(document.msExitFullscreen) {
+        document.msExitFullscreen();
+    }
+}
+
+function goFullScreen() {
+    if(videoContainer.requestFullscreen) {
+        videoContainer.requestFullscreen();
+    } else if(videoContainer.webkitRequestFullscreen) {
+        videoContainer.webkitRequestFullscreen();
+    } else if(videoContainer.mozRequestFullscreen) {
+        videoContainer.mozRequestFullscreen();
+    } else if(videoContainer.msRequestFullscreen) {
+        videoContainer.msRequestFullscreen();
+    }
+    fullScrnBtn.innerHTML = '<i class="fas fa-compress"></i>';
+    fullScrnBtn.removeEventListener("click", goFullScreen);
+    fullScrnBtn.addEventListener("click", exitFullScreen);
+}
+
 function init() {
     playBtn.addEventListener("click", handlePlayClick);
     volumeBtn.addEventListener("click", handleVolumeClick);
+    fullScrnBtn.addEventListener("click", goFullScreen);
 }
 
 if(videoContainer) {
