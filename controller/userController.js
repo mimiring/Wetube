7186473666f2e491ba5d40c423b505905e425ca4
mimiring <island_2@naver.com>
2020-02-18 21:53:29 +0900

    잘못된 id로 user 연결시 오류 확인(home으로 이동)

--- controller/userController.js
+++ controller/userController.js
@@ -87,6 +87,17 @@ export const getMe = (req, res) => {
 };
 
 export const users = (req, res) => res.render("Users", { pageTitle: "Users" });
-export const userDetail = (req, res) => res.render("userDetail", { pageTitle: "User Detail" });
+export const userDetail = async(req, res) => {
+	const {
+		params: { id }
+	} = req;
+	try {
+		const user = await User.findById(id);
+		res.render("userDetail", { pageTitle: "User Detail", user });
+	} catch(error) {
+		res.redirect(routes.home);
+	}
+};
+
 export const editProfile = (req, res) => res.render("editProfile", { pageTitle: "Edit Profile" });
 export const changePassword = (req, res) => res.render("changePassword", { pageTitle: "Change Password" });
