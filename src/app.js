53b5480c82e00b1437519a464982ecd94da07197
mimiring <island_2@naver.com>
2021-06-11 01:11:57 +0900

    pug 제거 후 설정 변경

--- src/app.js
+++ src/app.js
@@ -32,7 +32,6 @@ import "./passport";
 const app = express(); // const app = createApplication();
 
 app.use(helmet()); // response header에 보안관련된 항목을 추가해줌.
-app.set("view engine", "pug"); // ejs, pug등 view engine설정
 app.set("view engine", "ejs"); // ejs추가
 // app.set("views","views"); // view 폴더 지정.
 app.set("views", path.join(__dirname, "views"));
